64e6312f9db6dfc4307036844a7068cc4f7e9bca
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-04-17 15:36:24 +0300

    copy of

--- js/challenge.js
+++ js/challenge.js
@@ -1,58 +1,67 @@
-var timersCount = 0;
-var pause = false; //is timer paused
-
-countTimers();
-
-function countTimers() {
-  timersCount++;
-    
-  var count = 0;
-  var counter = setInterval(timer, 1000);
-
-  let minus = document.getElementById('minus');
-    minus.addEventListener('click',() => {
-        count = count -2;
-    });
-
-    let plus = document.getElementById('plus');
-    plus.addEventListener('click',() => {
-        count = count + 1;
-    })
-
-  function timer() {
-    if (!pause) { //do something if not paused
-      count = count + 1;
-      if (count < 0) {
-        clearInterval(counter);
-        setTimeout(countTimers, 5000); //start count from 26 again
-        return;
-      } 
-
-      document.getElementById("counter").innerHTML = count;
-    }
+"use strict";
+function _toConsumableArray(a){
+  if(Array.isArray(a)){
+    for(var b=0,c=Array(a.length);
+    b<a.length;b++)c[b]=a[b];
+    return c}
+      return Array.from(a)
   }
 
-//document.getElementById("countTimers").innerHTML = timersCount;
-}
+var playing=!0,
+timer=function(){
+  return setInterval(
+    function(){
+      var a=document.getElementById("counter"),
+      b=parseInt(a.innerText);
+      a.innerText=b+1},1e3)
+    },
 
-document.getElementById('pause').addEventListener('click', function () {
-	pause = true;
-    // if(pause) {
-    //     pauseInterval(counter);
-    // }else {
-    //     pause = false;
-    // }
-});
+interval=timer(),
 
-let list = document.getElementById('list');
+minus=document.getElementById("minus"),
+plus=document.getElementById("plus"),
+heart=document.getElementById("heart"),
+pause=document.getElementById("pause"),
+commentForm=document.getElementsByTagName("form")[0];
 
-let p = document.createElement('p');
-p.innerText = document.getElementById("user_input").value;
-let submit = document.getElementById('submit');
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    return p;
-    
-})
+minus.addEventListener("click",function(){
+  var a=document.getElementById("counter"),
+  b=parseInt(a.innerText);a.innerText=b-1}),
+  
+plus.addEventListener("click",function(){
+  var a=document.getElementById("counter"),
+  b=parseInt(a.innerText);a.innerText=b+1}),
 
-list.appendChild(p);
+heart.addEventListener("click",function(){
+  var a=document.getElementById("counter"),
+  b=parseInt(a.innerText),
+  c=document.querySelector(".likes"),
+  d=void 0;
+  if([].concat(_toConsumableArray(c.children))
+    .map(function(a){
+      return parseInt(a.dataset.num)})
+      .includes(b))
+    {d=document.querySelector('[data-num="'+b+'"]');
+    var e=parseInt(d.children[0].innerText);
+    d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"}
+  else
+    (d=document.createElement("li")).setAttribute("data-num",b),
+    d.innerHTML=b+" has been liked <span>1</span> time",
+    c.appendChild(d)}),
+    pause.addEventListener("click",function(){
+      playing?(playing=!1,
+      clearInterval(interval),
+      this.innerText="resume")
+      :
+      (playing=!0,interval=timer(),
+      this.innerText="pause"),
+      [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){
+        "pause"!==a.id&&(a.disabled=!playing)})}),
+      commentForm.addEventListener("submit",function(a){
+        a.preventDefault();
+        var b=this.children[0],
+        c=b.value;
+        b.value="";
+        var d=document.querySelector(".comments"),
+        e=document.createElement("p");e.innerText=c,d.appendChild(e)}
+        );
